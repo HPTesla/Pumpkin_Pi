b5a7b25022d84c240458fa555da907890320cb1b
Brian Gillespie <bgbriangillespie@gmail.com>
2012-10-13 18:24:30 -0400

    objects, yo
    Created an LED object, wrote the light sequences, cleaned up code,
    comments, license, README, yada yada. This is still untested; I'm
    waiting for some hardware to arrive in the mail.

--- README.md
+++ README.md
@@ -1,11 +1,29 @@
-Pumpkin_Pi
+Pumpkin Pi
 ==========
 
 Illuminate a Jack-o'-Lantern using LEDs and Raspberry Pi.
 
 Usage
-=====
+-----
 
-sudo ./pumpkinPi.py hh:mm
-hh:mm is the duration in hours in minutes to run. 
+**sudo ./pumpkinPi.py hh:mm**
 
+hh:mm is the duration in hours and minutes to run.
+
+This program must be run as root in order to access the GPIO pins.
+
+Author
+------
+
+Brian Gillespie, 2012
+
+  @begillespie
+  
+  github.com/begillespie
+
+License
+-------
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
--- pumpkinPi.py
+++ pumpkinPi.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python -tt
 #  PumpkinPi, an LED Jack O'Lantern for Raspberry Pi
+#  https://github.com/begillespie/Pumpkin_Pi
+
 #  To access the GPIO on the Raspberry Pi, you must run this program as root.
 
+#  by Brian Gillespie, 2012
+#     @begillespie
+#  GPL license, see README for more details.
+
 import RPi.GPIO as GPIO, sys, time, random, bigletters
-#  bigletters is a custom module that prints characters made of ascii art to the console. Call it using bigletters.write('message','color','style'). Run './bigletters.py --h' at the command prompt for a list of options.
 
-#  assign GPIO pins
-led = {
-'red':18, 
-'white1':23,
-'white2':24}
-motionsensor = 11
-
-#  variables
 debug = False
-flash_patterns = [1,2,3,4]    # This list is used to randomly call flashing patterns to make the jack o'lantern more interesting.
 
-pumpkin = ["\033[0;33m",           # Just a little fun
-r"                                .-'\ ",
+#  bigletters is a custom module that prints characters made of ascii art to the console. 
+#  Call it using bigletters.write('message','color','style').
+#  Run './bigletters.py --h' at the command prompt for a list of options.
+
+pumpkin = ["\033[0;33m",                    # Just a little fun. The color and escape sequences
+r"                                .-'\ ",   # should work in BASH. Can't guarantee any other shells.
 r"                                \:. \ ",
 r"                                |:.  \ ",
 r"                                /::'  \ ",
@@ -43,47 +43,121 @@ r"                  `\  :     `-.\/__\__\/_.;'   :   /` ",
 r"              jgs   `\  '   :   :        :   :  /` ",
 r"                      `-`.__`        :   :__.'-` ",
 r"                            `-..`.__.'..-` ",
-'\033[0m']      # Pumpkin ASCII art courtesy of The Internet. Thanks, jgs!!
+'\033[0m']    # Pumpkin ASCII art courtesy of The Internet. Thanks, jgs!!
+
+class LED:
+  """Access the LEDs with some simple methods"""
+  def __init__(self, pin):
+    self.pin = pin                      # Initialize the LED object with the pin number
+    leds.append(self)                   # The object appends itself to the led list,making
+#                                         it easy to perform actions on all the leds at once.
+    GPIO.setup(self.pin), GPIO.OUT)     # Set the GPIO pin
+  def on(self):
+    GPIO.output(self.pin, True)
+  def off(self):
+    GPIO.output(self.pin, False)
+
+leds = []     # Holds a list of all the LED objects
+
+RedLED = LED(18)
+White1LED = LED(23)
+White2LED = LED(24)
 
 #  set up GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(led['red'], GPIO.OUT)
-GPIO.setup(led['white1'], GPIO.OUT)
-GPIO.setup(led['white2'], GPIO.OUT)
 GPIO.setup(motionsensor, GPIO.IN)
 
-#  define some different LED flash patterns
-def flash_1():
+motionsensor = 11    # assign GPIO pin for the motion sensor
+      
+def pumpkin_pi_on():
+  if debug: print 'All on'
+  for i in range(len(leds)):
+    leds[i].on()
+
+def pumpkin_pi_off():
+  if debug: print 'All off'
+  for i in range(len(leds)):
+    leds[i].off()
+
+#  Define some different LED flash patterns. I tried to make each one around 1-2
+#  seconds long so the motion sensor gets polled every second or two.
+def flash_1(): 
   if debug: print 'Flash 1'
+  for i in range(6):
+    pumpkin_pi_on()
+    sleep.sleep(.25)
+    pumpkin_pi_off()
+    sleep.sleep(.25)
+
 def flash_2():
   if debug: print 'Flash 2'
+  RedLED.on()
+  for i in range(6):
+    White1LED.on()
+    sleep.sleep(.2)
+    White2LED.on()
+    sleep.sleep(.1)
+    White1LED.off()
+    White2LED.off()
+    sleep.sleep(.2)
+  RedLED.off()
+
 def flash_3():
   if debug: print 'Flash 3'
+  RedLED.on()
+  for i in range(8):
+    White1LED.on()
+    sleep(.1)
+    White1LED.off()
+    sleep(.15)
+  RedLED.off()
+
 def flash_4():
   if debug: print 'Flash 4'
+  for i in range(2):
+    White1LED.on()
+    sleep.sleep(.5)
+    White2LED.on()
+    sleep.sleep(.5)
+    White1LED.off()
+    sleep.sleep(.5)
+    White2LED.off()
+    sleep.sleep(.5)
 
-def motion_activate():
-  if debug:  print 'Motion Sensor Activate'
 
-def jackolantern():
-#  if GPIO.input(motionsensor):     # Check the motion sensor to show a special animation to visitors.
-#    motion_activate()
-#  else:
-    pattern = random.choice(flash_patterns)
-    if pattern == 1:
-      flash_1()
-    elif pattern == 2:
-      flash_2()
-    elif pattern == 3:
-      flash_3()
-    elif pattern == 4:
-      flash_4()
+# This list is used to randomly call flashing patterns. If you add any pattern
+# functions that you want in the rotation, add them to this list.
+flash_patterns = [flash_1, flash_2, flash_3, flash_4]     #<-- A list of functions!!
 
-def jackolantern_off():
-  if debug: print 'All off'
-  for each in led:
-    GPIO.output(led[each], False)
-#  GPIO.cleanup()
+def motion_sequence():     #  Special sequence to call on motion sensor activation
+  print 'Motion Sensor Activate'  #debug
+  pumpkin_pi_off()
+  time.sleep(1)
+  pumpkin_pi_on()
+  time.sleep(2)
+  # do some more stuff
+
+def pumpkin_pi_quit():
+      print 'Exit PumpkinPi\n\n'
+      for i in range(4):
+        pumpkin_pi_on()
+        sleep.sleep(1)
+        pumpkin_pi_off()
+        sleep.sleep(1)
+      GPIO.cleanup()
+      bigletters.write('      happy', 'yellow', 'normal')     # Print an adorable exit message
+      bigletters.write(' halloween!!!', 'yellow', 'normal')
+      sys.exit(0)
+      
+def pumpkin_pi(stopTime):
+  while stopTime > time.time():
+    if GPIO.input(motionsensor): # Check the motion sensor to show a special animation to visitors.
+      motion_sequence()
+    else:
+      random.choice(flash_patterns)()  # Randomly choose a function from the list and execute it.
+      pumpkin_pi_off()
+  pumpkin_pi_quit()
+
 
 def main():
   helpMessage = 'Usage: sudo . /pumpkinPi REQUIRED duration:[hh:mm] OPTIONAL debug mode:{--d}'
@@ -99,28 +173,19 @@ def main():
     sys.exit(1)
   else:
     hour = int(sys.argv[1][:2])
-    minute = int(sys.argv[1][-2:])
-    stopTime = time.time() + (hour * 3600 + minute * 60)     #  Convert the duration to seconds and add it to the current time to find the stop time.
+    minute = int(sys.argv[1][-2:])                     # Convert the duration to seconds and add to
+    stopTime = time.time() + (hour * 3600 + minute * 60)  # the current time to find the stop time.
+
     print 'Jack-O-Lantern will go out at', time.strftime('%H:%M, %B %d, %Y', time.localtime(stopTime))
     print '\033[1;31mUse ctrl+C to exit early.\033[0m'
-
+    
   for line in pumpkin:    # Print a pumpkin to the console.
     print line
   bigletters.write(r' pumpkin*', 'purple', 'bold')
 
   stopTime = time.time() + 5 #debug
-
-  while True:     # Loop the code until time runs out.
-    if stopTime > time.time():
-      jackolantern()
-    else:
-      print 'Exit PumpkinPi\n\n'
-      jackolantern_off()     # Turn off all the LEDs
-      bigletters.write('      happy', 'yellow', 'normal')     # Print an adorable exit message
-      bigletters.write(' halloween!!!', 'yellow', 'normal')
-      sys.exit(0)
+  pumpkin_pi(stopTime)
 
 if __name__ == '__main__':
   print 'PumpkinPi starting'
   main()
-
